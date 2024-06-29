32b2739c28bf2c5ee844344d4774d376180b9376
EduardoSangaletti <eduardosangaletti@gmail.com>
2024-06-28 22:02:22 -0300

    bottomNavigation

--- app/src/main/java/com/example/ddm_trabalho3/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/ddm_trabalho3/ui/components/BottomNavigationBar.kt
@@ -7,6 +7,9 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.compose.material3.*
+import androidx.compose.ui.res.painterResource
+import com.example.ddm_trabalho3.R
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
@@ -14,15 +17,21 @@ fun BottomNavigationBar(navController: NavController) {
         BottomNavigationItem(
             selected = navController.currentDestination?.route == "inicialScreen",
             onClick = { navController.navigate("inicialScreen") },
-            icon = { /* Icon */ },
-            label = { androidx.compose.material3.Text("Inicial Screen") }
+            icon = { Icon(
+                painter = painterResource(id = R.drawable.casa100),
+                contentDescription = "Icone casa"
+            ) },
+            label = { androidx.compose.material3.Text("Início") }
         )
 
         BottomNavigationItem(
             selected = navController.currentDestination?.route == "perfilScreen",
             onClick = { navController.navigate("perfilScreen") },
-            icon = { /* Icon */ },
-            label = { androidx.compose.material3.Text("Perfil Screen") }
+            icon = { Icon(
+                painter = painterResource(id = R.drawable.perfil100),
+                contentDescription = "Icone perfil"
+            ) },
+            label = { androidx.compose.material3.Text("Perfil") }
         )
     }
 }
